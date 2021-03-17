5c10f2d8096ee4307e3405eddb735a8b2a7e6c27
psalmsglobal <psalmsglobal.0998@gmail.com>
2021-03-16 18:48:47 -0700

    Updated

--- classroomquiz/settings.py
+++ classroomquiz/settings.py
@@ -94,6 +94,16 @@ DATABASES = {
         }
 }
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'Student_Quiz_Appdb',
+#        'USER': 'postgres',
+#        'PASSWORD': 'gary1234',
+#        'HOST': 'localhost',
+#        'PORT':'5432',
+#        }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
