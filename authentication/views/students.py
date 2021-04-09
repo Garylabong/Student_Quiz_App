735d4c8d759e2a36bd22fa7027f5fc2f1ba591f2
psalmsglobal <psalmsglobal.0998@gmail.com>
2021-04-09 13:24:23 -0500

    Updated

--- authentication/views/students.py
+++ authentication/views/students.py
@@ -122,7 +122,7 @@ def take_quiz(request, pk):
                     correct_answers = student.quiz_answers.filter(answer__question__quiz=quiz, answer__is_correct=True).count()
                     score = round((correct_answers / total_questions) * 100.0, 2)
                     TakenQuiz.objects.create(student=student, quiz=quiz, score=score)
-                    if score < 50.0:
+                    if score < 60.0:
                         messages.warning(request, 'Better luck next time! Your score for the quiz %s was %s.' % (quiz.name, score))
                     else:
                         messages.success(request, 'Congratulations! You completed the quiz %s with success! You scored %s points.' % (quiz.name, score))
